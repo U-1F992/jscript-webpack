fa095c0fe007e99ffdbc5580610f72f860707410
Koutaro Mukai <mukai.k1011k@outlook.jp>
2022-12-22 21:22:04 +0900

    try chalk, but failed

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { fuga } from "./lib";
+import chalk from "chalk";
+import { fuga, useJson } from "./lib";
 
 const hogehoge = ["hoge", "fuga", "piyo"];
 hogehoge.forEach(async value => await fuga(value));
 
-WScript.Echo(JSON.stringify(hogehoge));
+WScript.Echo(chalk.red(useJson(hogehoge)));
--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,7 @@
 export async function fuga(piyo: string) {
     WScript.Echo(piyo);
 }
+
+export function useJson(value: any) {
+    return JSON.stringify(value);
+}
