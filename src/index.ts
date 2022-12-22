461cfea668e529959e64eb1dd047b31386f5a50f
Koutaro Mukai <mukai.k1011k@outlook.jp>
2022-12-23 00:02:35 +0900

    Comment out

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import chalk from "chalk";
+// import chalk from "chalk";
 import { fuga, useJson } from "./lib";
 
 const hogehoge = ["hoge", "fuga", "piyo"];
 hogehoge.forEach(async value => await fuga(value));
 
-WScript.Echo(chalk.red(useJson(hogehoge)));
+// WScript.Echo(chalk.red(useJson(hogehoge)));
+WScript.Echo(useJson(hogehoge));
