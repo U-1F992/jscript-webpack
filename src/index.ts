602f2de7015025cad8585704ef7802e0ce6bdb9a
Koutaro Mukai <mukai.k1011k@outlook.jp>
2022-12-23 10:23:18 +0900

    window

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // import chalk from "chalk";
+import "./window";
 import { fuga, useJson } from "./lib";
 
 const hogehoge = ["hoge", "fuga", "piyo"];
@@ -6,3 +7,10 @@ hogehoge.forEach(async value => await fuga(value));
 
 // WScript.Echo(chalk.red(useJson(hogehoge)));
 WScript.Echo(useJson(hogehoge));
+
+let id = setTimeout(() => WScript.Echo(`running on ${id}`), 1000);
+WScript.Echo("sleeping");
+WScript.Sleep(2000);
+
+id = setInterval(arg => WScript.Echo(`arg: ${arg}`), 100, "hoge");  // expected "hoge", but "undefined"
+WScript.Sleep(1000);
